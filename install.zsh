c7c9260719f3fe307459371c1122c1b82912e1c0
Omer Hershkovitz <hershkovitzomer@gmail.com>
2024-07-19 11:08:51 +0300

    added backup to install script

--- install.zsh
+++ install.zsh
@@ -1,9 +1,35 @@
 #!/bin/zsh
 # WIP
 
+# create backups of all configs
+FOLDERS_TO_EXCLUDE=".git"
+FILES_TO_EXCLUDE="README.md install.zsh"
+
+# Convert space-separated lists into arrays
+folders_array=(${=FOLDERS_TO_EXCLUDE})
+files_array=(${=FILES_TO_EXCLUDE})
+
+# Construct the find command with exclusion parameters
+find_command="find . -type f"
+for folder in "${folders_array[@]}"; do
+    find_command+=" -not -path \"./$folder/*\""
+done
+for file in "${files_array[@]}"; do
+    find_command+=" -not -name \"$file\""
+done
+
+eval $find_command | while read file; do
+  full_path="$HOME/${file#./}"
+  if [[ -f "$full_path" ]]; then
+    echo $full_path
+    mv $full_path "$full_path.bck"
+  fi
+done
+
+
 # install tpm
 if [[ ! -d ~/.tmux ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
-  echo "[WARNING] tpm is already installed";
+  echo "[INFO] tpm is already installed";
 fi
